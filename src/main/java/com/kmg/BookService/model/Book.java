64e3fc54f8f743dc14ff0d07ff079b651647c7ba
kmg22 <ssukmg22@gmail.com>
2026-01-20 11:32:48 +0900

    feat : Book entity 구현

--- src/main/java/com/kmg/BookService/model/Book.java
+++ src/main/java/com/kmg/BookService/model/Book.java
@@ -1,5 +1,29 @@
 package com.kmg.BookService.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="books")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Book {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String isbn;
+
+    private String author;
+    private String publisher;
+    private String pubDate;
+
+    @Column(length = 1000)
+    private String description;
 }
